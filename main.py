301c4135eaf75db6659ea7ec37cc39c7f31b3976
aniketpatil <your-aniketpatil1121@gmail.com>
2025-07-03 18:00:46 +0530

    commit this

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
 
 
-app = FastAPI()
+app = FastAPI() 
+
+def load_data():
+    import json
+    with open('patients.json', 'r') as file:
+        return json.load(file)
 
 @app.get("/")
 def hello():
-    return {"message": "Hello, World!"}
+    return {"message": "paitent management system API"}
 
 @app.get('/about')
 def about():
-    return {"message": "This is a simple FastAPI application."}
+    return {"message": "An API for managing patient data, including BMI calculations and health verdicts."} 
+
+@app.get('/view')
+def view():
+    data = load_data()
+    return {"patients": data}
+         
